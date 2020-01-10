4938ccf4f9a3d068b53f1e12eaad5b76919d7391
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-10 13:40:48 -0800

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training/training.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 const routes: Routes = [
-  {path: '', component: WelcomeComponent},
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
 ];
 
 @NgModule({
