0567f63a2f3c4cf62e598cc90a6b7d75b8dfc673
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-11 15:21:43 -0800

    Dive deep Material: Toolbar, Sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule,
-  MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule],
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule]
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule]
 })
 export class MaterialModule { }
