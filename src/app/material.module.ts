78d1537c836fa890239035a0c2f593b9e6642b20
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-11 16:31:26 -0800

    Add side nav menu list

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule,
-  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule],
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule, MatListModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule]
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule, MatListModule]
 })
 export class MaterialModule { }
