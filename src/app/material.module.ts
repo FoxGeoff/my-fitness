a5ca42607652e2d39f3262f5ff56008489143c4f
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-12 15:49:46 -0800

    Add tabs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule,
-  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule } from '@angular/material';
+import {
+  MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule,
+  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule
+} from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule, MatListModule],
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule,
+    MatListModule, MatTabsModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule, MatListModule]
+    MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule,
+    MatListModule, MatTabsModule]
 })
 export class MaterialModule { }
