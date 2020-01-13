24d0e63285fcdf51b0bb0be219fb1ea7832f5d94
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-12 16:23:28 -0800

    Add new-training card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import {
   MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule,
-  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule
+  MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatCardModule
 } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule,
-    MatListModule, MatTabsModule],
+    MatListModule, MatTabsModule, MatCardModule],
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatSidenavModule, MatSidenavModule, MatToolbarModule,
-    MatListModule, MatTabsModule]
+    MatListModule, MatTabsModule, MatCardModule]
 })
 export class MaterialModule { }
