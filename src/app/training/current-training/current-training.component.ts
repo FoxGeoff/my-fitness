e0a555f627202db1a685feb62ff82e5c467bf218
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-15 17:03:08 -0800

    Consume dialog output

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from '../stop-training/stop-training.component';
 
@@ -8,12 +8,17 @@ import { StopTrainingComponent } from '../stop-training/stop-training.component'
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
@@ -21,7 +26,6 @@ export class CurrentTrainingComponent implements OnInit {
       }
     }, 1000);
   }
-
   onStop() {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
@@ -29,8 +33,12 @@ export class CurrentTrainingComponent implements OnInit {
         progress: this.progress
       }
     });
-    dialogRef.afterClosed().subscribe( result => {
-      console.log(result);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
     });
 
   }
