0442d465bb145b4d4dec06be15a7aaa9138df715
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-15 22:21:50 -0800

    Add Data Services for Auth, and Training (dummy)

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+
+export class AuthService {
+  private user: User;
+
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 1000).toString()
+    };
+  }
+
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 1000).toString()
+    };
+  }
+
+  logout() {
+
+  }
+}
