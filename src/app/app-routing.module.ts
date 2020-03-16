c7a1eca50dbf105e0099771dca7398803f2a4056
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-03-16 16:16:56 -0700

    Add Training Route Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,20 @@ import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training/training.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
+import { AuthGuard } from './auth/auth.guard';
+
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  // This is an exception for providers
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
