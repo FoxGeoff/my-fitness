8f3fc7357db68d65d8c3e5ca6d9592c99257b325
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-03-25 16:33:50 -0700

    Setting an Active Exercise

--- src/app/training.service.ts
+++ src/app/training.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Exercise } from './exercise.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
+  activeExercise = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
     {id: 'touch-toes', name: 'Touch-toes', duration: 180, calories: 10},
     {id: 'side-lunges', name: 'Side-lunges', duration: 120, calories: 5},
     {id: 'burpees', name: 'Burpees', duration: 60, calories: 8},
   ];
-  private runningExercise: Exercise;
+  private selectedExercise: Exercise;
 
   constructor() { }
 
@@ -20,7 +23,9 @@ export class TrainingService {
   }
 
   startExercise(selectedId: string) {
-    const selectedExercise = this.availableExercises.find( ex => ex.id === selectedId);
-    this.runningExercise = selectedExercise;
+    this.selectedExercise =
+      this.availableExercises.find( ex => ex.id === selectedId);
+
+    this.activeExercise.next(this.selectedExercise);
   }
 }
